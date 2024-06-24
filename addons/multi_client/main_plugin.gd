a6dc2bb0d495de24b3484f2e4afb3d49bed8584f
brzuskov <wojciech.brzuszkiewicz@hotmail.com>
2024-06-24 23:12:23 +0200

    Added NumberLineEdit

--- addons/multi_client/main_plugin.gd
+++ addons/multi_client/main_plugin.gd
@@ -2,10 +2,31 @@
 extends EditorPlugin;
 class_name MultiClientPlugin;
 
-const main_plugin_scene: StringName;
+const _main_plugin_scene_path: StringName = "";
+var _main_control_node: Control = null;
+var _is_initialized: bool = false;
 
 func _enter_tree():
-	pass
+	__initialize_plugin();
+		
 	 
 func _exit_tree():
-	pass
+	if !_is_initialized:
+		return;
+		
+	__dispose_plugin();
+	
+	
+func __initialize_plugin() -> void:
+	var plugin_packed_scene: PackedScene = load(_main_plugin_scene_path);
+	
+	if !plugin_packed_scene:
+		printerr("Could not load plugin scene, path: %s" % _main_plugin_scene_path);
+		return;
+		
+	
+
+
+func __dispose_plugin() -> void:
+	pass;
+
