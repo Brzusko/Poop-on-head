6f064ed639e28eb046602cbe857e8705cef8d7bf
brzuskov <wojciech.brzuszkiewicz@hotmail.com>
2024-06-26 00:03:26 +0200

    Saving and loading data from file

--- addons/multi_client/scripts/multi_client_tool_settings.gd
+++ addons/multi_client/scripts/multi_client_tool_settings.gd
@@ -1,6 +1,53 @@
-extends Node;
+extends Object;
 class_name MultiClientToolSettings;
 
-const _settings_file_path: String = "user://multi_client_tool/settings.json";
+const _settings_dir_path: String = "user://multi_client_tool";
+const _settings_file_name: String = "settings.json";
+const _settings_file_path: String = _settings_dir_path + "/" + _settings_file_name;
 
-func load_settings() -> Dictionary:
+func load_settings() -> MultiClientSaveData:
+	var loaded_save_data: MultiClientSaveData = MultiClientSaveData.new();
+
+	if DirAccess.dir_exists_absolute(_settings_dir_path) && FileAccess.file_exists(_settings_file_path):
+		var file: FileAccess = FileAccess.open(_settings_file_path, FileAccess.READ);
+
+		if !file:
+			printerr("Could not open save file located in: %s, with error: %s" % [_settings_file_path, FileAccess.get_open_error()]);
+			return loaded_save_data;
+
+		var file_content: Dictionary = JSON.parse_string(file.get_as_text());
+
+		if !file_content:
+			printerr("Could not parse file located in %s" % _settings_file_path);
+			return loaded_save_data;
+		
+		loaded_save_data = MultiClientSaveData.from_dict(file_content);
+		return loaded_save_data;
+
+	__generate_save_file();
+	return loaded_save_data;
+	
+func __generate_save_file() -> void:
+	if !DirAccess.dir_exists_absolute(_settings_dir_path):
+		var result: int = DirAccess.make_dir_absolute(_settings_dir_path);
+
+		if result != OK:
+			printerr("Could not create save directory at path: %s" % _settings_dir_path);
+			return;
+	
+	if !FileAccess.file_exists(_settings_file_path):
+		var empty_data: MultiClientSaveData = MultiClientSaveData.new();
+		save_data(empty_data);
+
+
+func save_data(data_to_save: MultiClientSaveData) -> bool:
+	var file_content: String = JSON.stringify(data_to_save.to_dict());
+	var file: FileAccess = FileAccess.open(_settings_file_path, FileAccess.WRITE);
+
+	if !file:
+		printerr("Could not write save file located in: %s, with error: %s" % [_settings_file_path, FileAccess.get_open_error()]);
+		return false;
+
+	file.store_string(file_content);
+
+	return true;
--- addons/multi_client/scripts/settings_data.gd
+++ addons/multi_client/scripts/settings_data.gd
@@ -1,5 +1,56 @@
 extends RefCounted;
 class_name MultiClientSaveData;
 
-var cmd_string: String;
-var client_count: int;
+const dict_keys: Array[String] = [
+	"cmd_string",
+	"client_count",
+	"start_from_main_scene",
+];
+
+var cmd_string: String = "": set = _cmd_string_setter;
+
+func _cmd_string_setter(value: String) -> void:
+	self.set_meta("cmd_string", value);
+	cmd_string = value;
+
+var client_count: int = 0: set = _client_count_setter;
+
+func _client_count_setter(value: int) -> void:
+	if value < 0:
+		return;
+	
+	self.set_meta("client_count", value);
+	client_count = value;
+
+var start_from_main_scene: bool = false: set = _start_from_main_scene;
+
+func _start_from_main_scene(value: bool) -> void:
+	self.set_meta("start_from_main_scene", value);
+	start_from_main_scene = value;
+ 
+func _init():
+	for key: String in dict_keys:
+		self.set_meta(key, self.get(key));
+
+func to_dict() -> Dictionary:
+	var new_dict: Dictionary = {};
+
+	for key: String in dict_keys:
+		if !self.has_meta(key):
+			continue;
+		
+		new_dict[key] = self.get_meta(key);
+
+	return new_dict;
+
+static func from_dict(data_to_parse: Dictionary) -> MultiClientSaveData:
+	var save_data: MultiClientSaveData = MultiClientSaveData.new();
+
+	# for now, tool does not support nested data in file like arrays/dictionaries
+	for key: String in data_to_parse.keys():
+		if !save_data.has_meta(key):
+			continue;
+		
+		save_data.set(key, data_to_parse[key]);
+	
+	return save_data;
